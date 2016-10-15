6b2e77bd3c6b01fcc25fa0ac6254cd41ca3eaa68
Guilherme Borba <Brobz@Guilhermes-MacBook-Pro.local>
2016-10-14 22:28:21 -0500

    update

--- client/src/js/menu.js
+++ client/src/js/menu.js
@@ -5,8 +5,6 @@ var connectedText = document.getElementById("connectedText");
 var canvasElement = document.getElementById("canvas");
 var canvas = document.getElementById("canvas").getContext("2d");
 
-canvasElement.style.display = "none"
-
 canvas.font = "15px Monaco";
 
 function connected(data){
@@ -19,7 +17,7 @@ function connected(data){
   nameInput.style.display = "none";
   connectButton.style.display = "none";
   canvasElement.style.display = "";
-  
+
 
 }
 
