8a1ee4d80abb31db18be5bdeeedc6d33838193f1
Guilherme Borba <Brobz@Guilhermes-MacBook-Pro.local>
2016-10-14 20:31:51 -0500

    Update

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 
+var port = Number(process.env.PORT || 3000);
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/client/index.html");
 });
 
 app.use("/client", express.static(__dirname + "/client"));
 
-server.listen(2000);
+server.listen(process.env.PORT || 2000);
 
 console.log("Server Ready!");
 
