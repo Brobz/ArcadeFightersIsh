53ba62fd5735db464ab07f0bf804af17924ef100
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2021-12-15 22:14:09 -0600

    Add power-up to get small

--- src/server/power_up/index.ts
+++ src/server/power_up/index.ts
@@ -3,17 +3,19 @@ import BigBullets from "./big_bullets";
 import ClusterGun from "./cluster_gun";
 import Heal from "./heal";
 import IncreasedFireRate from "./increased_fire_rate";
+import Mini from "./mini";
 import MultiGun from "./multi_gun";
 import Shield from "./shield";
 import Speed from "./speed";
 
 export const AVAILABLE_POWER_UPS = [
+  AvoidBlocks,
   BigBullets,
   ClusterGun,
   Heal,
   IncreasedFireRate,
+  Mini,
   MultiGun,
   Shield,
   Speed,
-  AvoidBlocks,
 ]
