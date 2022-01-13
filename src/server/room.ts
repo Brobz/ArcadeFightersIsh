ad3bd787660ff30bd35dba97c1f82d12972c955e
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-01-13 13:39:22 -0600

    Fix error in showing room info

--- src/server/room.ts
+++ src/server/room.ts
@@ -55,7 +55,7 @@ export default class Room {
     this.info = `Mode: ${this.teamBased ? "TDM" : "FFA"}<br>`;
     this.info += `Players: ${this.players.length} / ${this.maxSize}<br>`;
     const difference = this.minSize - this.players.length;
-    if (difference >= 0) {
+    if (difference <= 0) {
       this.info += '<span class="text-success"> Game ready to start!</span>';
     } else {
       this.info += `<span class="text-danger">(${difference} more player needed for game start)</span>`;
