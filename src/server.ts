0e0dd224ef93a4eea4c1e41e2aef3adc154cd91c
Guilherme Bittencourt de Borba <gui.bittborba@gmail.com>
2022-02-10 14:09:38 -0600

    adds 404.html page and eror 404 routing to catch any unknown routes and serve them the 404 page

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,18 @@ export default function createServer() {
   const app = express();
   const server = new Server(app);
 
+  // Serve all site contents from the /client folder from default route "/"
   app.use(express.static(__dirname + "/client"));
 
+  //The 404 Route (must be the last route on the file)
+  app.get('*', (req : any, res : any) => {
+    res.sendFile(__dirname + "/client/404.html");
+  });
+
+  // Start the server (listen on specified port for incoming connections)
   server.listen(process.env.PORT || 5000);
 
+  // All done!
   console.log(">> Server Ready!");
 
   return server;
